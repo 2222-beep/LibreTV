0959d91b8f041e1adfbf8d3faef03063e1156ddc
2222 <156060275+2222-beep@users.noreply.github.com>
2025-07-19 17:03:50 +0800

    Update version-check.js

--- js/version-check.js
+++ js/version-check.js
@@ -1,2 +1,1 @@
-<!--cloudflare 分析 -->
-Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "cf61e5c986e449dcb82383a6bf39b6c4"}'></script><!-- End Cloudflare Web Analytics 
+
